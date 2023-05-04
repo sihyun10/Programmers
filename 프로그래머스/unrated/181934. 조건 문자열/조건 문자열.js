15e5347a8fca8d23db6370be86fd7f19028ec5c0
sihyun <hyhy200170@gmail.com>
2023-05-05 01:33:13 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+    let answer = 0;
+    
+    if (ineq === ">") {
+        if (eq === "=") {
+            answer = (n >= m) ? 1 : 0;
+        } else if (eq === "!") {
+            answer = (n > m) ? 1 : 0;
+        }
+    } else if (ineq === "<") {
+        if (eq === "=") {
+            answer = (n <= m) ? 1 : 0;
+        } else if (eq === "!") {
+            answer = (n < m) ? 1 : 0;
+        }
+    }
+    return answer;
+}
