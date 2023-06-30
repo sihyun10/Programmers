f355b94abcd506591794129bfbf055426886bd16
sihyun <hyhy200170@gmail.com>
2023-06-30 15:21:44 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,11 @@
+function solution(n, numlist) {
+  let result = [];
+  
+  for (let i = 0; i < numlist.length; i++) {
+    if (numlist[i] % n === 0) {
+        result.push(numlist[i]);
+    }
+  }
+  
+  return result;
+}
