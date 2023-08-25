e4830d5d2b5b44914e4aa2ebb2c38267a6fb4b75
sihyun <hyhy200170@gmail.com>
2023-08-25 18:27:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,15 +1,9 @@
-// 다음 라운드에 진출할 참가자의 번호는 다시 1번부터 N/2번을 차례대로 배정받습니다.
-// (조건) a,b 선수는 항상 이긴다.
-// a,b선수가 맞붙게 되는 라운드를 구해야하기에 같아지는 순간까지 구하면됨
-// Math.ceil() : 소수점 올림
+function solution(n,a,b)
+{
+    var answer = 0;
 
-function solution(n,a,b) {
-    let answer = 1;
-    
-    while(Math.ceil(a/2) !== Math.ceil(b/2)) {
-        a = Math.ceil(a/2);
-        b = Math.ceil(b/2);
-        answer++;
-    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
     return answer;
 }
