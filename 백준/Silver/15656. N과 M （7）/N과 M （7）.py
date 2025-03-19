3c7a83b11955ad024da47bd65cc6d8322eddb191
sihyun <hyhy200170@gmail.com>
2025-03-19 19:54:30 +0900

    [Silver III] Title: N과 M (7), Time: 1232 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+data = list(map(int, sys.stdin.readline().split()))
+data.sort()
+
+answer = []
+
+def back():
+  if len(answer) == M:
+    print(" ".join(map(str, answer)))
+    return
+
+  for i in range(N):
+    answer.append(data[i])
+    back()
+    answer.pop()
+
+back()
