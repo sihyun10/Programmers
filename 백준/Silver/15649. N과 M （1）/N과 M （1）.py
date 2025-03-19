2997b420f32599645f083b4306f82fd9a31c5ea8
sihyun <hyhy200170@gmail.com>
2025-03-19 13:54:17 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+answer = []
+
+def back():
+  if len(answer) == M:
+    print(" ".join(map(str, answer)))
+    return
+
+  for i in range(1, N+1):
+    if i not in answer:
+      answer.append(i)
+      back()
+      answer.pop()
+
+
+back()
