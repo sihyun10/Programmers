b50d6a0f45856b433f8ae70f41048c1c66a2af56
sihyun <hyhy200170@gmail.com>
2025-04-08 21:43:15 +0900

    [Gold V] Title: 회의실 배정, Time: 260 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -5,16 +5,13 @@ N = int(input()) #회의의 수
 meeting_inf = [list(map(int, input().strip().split())) for _ in range(N)]
 meeting_inf.sort(key=lambda x: (x[1], x[0])) # 끝나는 시간을 기준으로 내림차순
 
-meet_usage_table = []
 end_time = 0
+count = 0
 
+# (수정) -> meet_usage_table 배열에 회의 정보를 저장 x -> 회의 수를 바로 셀 수 있도록
 for meeting in meeting_inf:
-  if len(meet_usage_table) == 0:
-    meet_usage_table.append(meeting)
+  if meeting[0] >= end_time:
+    count += 1
     end_time = meeting[1]
   
-  elif meeting[0] >= end_time:
-    meet_usage_table.append(meeting)
-    end_time = meeting[1]
-  
-print(len(meet_usage_table))
+print(count)
