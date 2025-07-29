4e0d9e24879f7d83151a75f807e11114b84ed1fa
sihyun <hyhy200170@gmail.com>
2025-07-29 13:46:09 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 20704 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * 입력 개수 N을 받는다.
+ * N번 반복하면서 명령어를 읽는다.
+ * 각 명령어를 switch문으로 구분 처리
+ * 출력은 StringBuilder에 저장했다가 마지막에 한 번에 출력
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+
+            String space = " ";
+            String[] parts = input.split(space);
+            String command = parts[0];
+
+            switch (command) {
+                case "push":
+                    int x = Integer.parseInt(parts[1]);
+                    queue.addFirst(x);
+                    break;
+                case "pop":
+                    if (appendIfEmpty(queue, sb)) break;
+                    sb.append(queue.pollFirst()).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "top":
+                    if (appendIfEmpty(queue, sb)) break;
+                    sb.append(queue.getFirst()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    private static boolean appendIfEmpty(Deque<Integer> queue, StringBuilder sb) {
+        if (queue.isEmpty()) {
+            sb.append(-1).append("\n");
+            return true;
+        }
+        return false;
+    }
+}
